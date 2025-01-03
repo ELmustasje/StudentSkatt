dc8a392b83a49d1aa7ee150166db037d07ff830b
ELmustasje <thomasbarth@live.no>
2025-01-03 14:54:59 +0100

    added togglebutton for phone

--- src/assets/comonents/Navbar.tsx
+++ src/assets/comonents/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light py-4">
       <div className="container-fluid">
@@ -10,6 +10,17 @@ const Navbar = () => {
           alt="Logo"
           style={{ height: "60px", width: "auto" }}
         />
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
         <div
           className="collapse navbar-collapse justify-content-center"
           id="navbarSupportedContent"
@@ -25,21 +36,48 @@ const Navbar = () => {
                 Guide
               </a>
             </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="/"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
                 Sider
               </a>
-              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="bsusparing">Bsu</a></li>
-                <li><a class="dropdown-item" href="fond">Fond</a></li>
-                <li><a class="dropdown-item" href="formuegrense">Formuegrense</a></li>
-                <li><a class="dropdown-item" href="generelle-tips">Generelle tips</a></li>
-                <li><a class="dropdown-item" href="inntektsgrense">Inntektsgrense</a></li>
-                <li><a class="dropdown-item" href="nettoformue">Nettoformue</a></li>
-                <li><a class="dropdown-item" href="reise-fradrag">Reisefradrag</a></li>
-                <li><a class="dropdown-item" href="rente-fradrag">Rentefradrag</a></li>
-                <li><a class="dropdown-item" href="skattefritt-aksjer">Skattefri aksjegevinst</a></li>
-                <li><a class="dropdown-item" href="utgiftsfradrag">Utgiftsfradrag</a></li>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <a className="dropdown-item" href="bsusparing">Bsu</a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="fond">Fond</a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="formuegrense">Formuegrense</a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="generelle-tips">Generelle tips</a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="inntektsgrense">Inntektsgrense</a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="nettoformue">Nettoformue</a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="reise-fradrag">Reisefradrag</a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="rente-fradrag">Rentefradrag</a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="skattefritt-aksjer">Skattefri aksjegevinst</a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="utgiftsfradrag">Utgiftsfradrag</a>
+                </li>
               </ul>
             </li>
           </ul>
